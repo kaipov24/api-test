d685cfcbb6dfe3b4d1efda9e1b96e6373856e242
kaipov24 <kaipovich24@gmail.com>
2025-06-11 20:50:27 +0600

    Connected

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/kaipov24/scaffold-api/internal/api"
 	"github.com/kaipov24/scaffold-api/internal/store"
+	"github.com/kaipov24/scaffold-api/migrations"
 )
 
 type Application struct {
@@ -22,6 +23,12 @@ func NewApplication() (*Application, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to run migrations: %v", err))
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	workoutHandler := api.NewWorkoutHandler()
